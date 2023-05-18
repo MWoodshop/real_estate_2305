55abff846daf370b931f8154a06b2f7e3ae11359
Mike Wood <mikewwood@icloud.com>
2023-05-18 12:08:06 -0400

    Test: creates two room instances and test variables.

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -20,5 +20,20 @@ RSpec.describe Room do
       house = House.new('$400000', '123 sugar lane')
       expect(house.rooms).to eq([])
     end
+
+    it 'returns correct values when creating two rooms' do
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+
+      expect(room_1).to be_a Room
+      expect(room_1.category).to eq(:bedroom)
+      expect(room_1.length).to eq(10)
+      expect(room_1.width).to eq('13')
+
+      expect(room_2).to be_a Room
+      expect(room_2.category).to eq(:bedroom)
+      expect(room_2.length).to eq(11)
+      expect(room_2.width).to eq('15')
+    end
   end
 end
--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -1,24 +1,38 @@
 require './lib/room'
+require 'pry'
 
+# Iteration 1
 RSpec.describe Room do
-  describe "Iteration 1" do
-    it "exists" do
+  describe 'Iteration 1' do
+    it 'exists' do
       room = Room.new(:bedroom, 10, '13')
       expect(room).to be_a Room
     end
 
-    xit "it has a category" do
+    it 'it has a category' do
       room = Room.new(:bedroom, 10, '13')
-
       expect(room.category).to eq(:bedroom)
     end
 
-    xit "can get area" do
+    it 'can get area' do
       room1 = Room.new(:bedroom, 10, '13')
       room2 = Room.new(:living_room, 15, '12')
 
       expect(room1.area).to eq(130)
       expect(room2.area).to eq(180)
+    end
+
+    it 'starts unpainted' do
+      room = Room.new(:bedroom, 10, '13')
+
+      expect(room.is_painted?).to eq(false)
+    end
+
+    it 'paint method should cause is_painted? to return true' do
+      room = Room.new(:bedroom, 10, '13')
+      room.paint
+
+      expect(room.is_painted?).to eq(true)
     end
   end
 end
