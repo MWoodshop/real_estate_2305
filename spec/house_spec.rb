2a7d29602870d0362b8c1665c98d2c631eb17890
Mike Wood <mikewwood@icloud.com>
2023-05-18 12:29:07 -0400

    Test: test add_room method on four rooms

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -61,6 +61,20 @@ RSpec.describe Room do
 
         expect(house.above_market_average?).to eq(true)
       end
+
+      it 'adds four rooms with add_room method' do
+        house = House.new('$400000', '123 sugar lane')
+        room_1 = Room.new(:bedroom, 10, '13')
+        room_2 = Room.new(:bedroom, 11, '15')
+        room_3 = Room.new(:living_room, 25, '15')
+        room_4 = Room.new(:basement, 30, '41')
+        house.add_room(room_1)
+        house.add_room(room_2)
+        house.add_room(room_3)
+        house.add_room(room_4)
+
+        expect(house.rooms).to eq([room_1, room_2, room_3, room_4])
+      end
     end
   end
 end
