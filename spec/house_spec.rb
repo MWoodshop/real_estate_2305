e6a5ff100c5d69ea2d2cd1643df5768e1fc1c087
Mike Wood <mikewwood@icloud.com>
2023-05-18 12:32:45 -0400

    Test: add rooms_from_category test 1

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -75,6 +75,20 @@ RSpec.describe Room do
 
         expect(house.rooms).to eq([room_1, room_2, room_3, room_4])
       end
+
+      it 'adds four rooms and returns correct rooms from category tested' do
+        house = House.new('$400000', '123 sugar lane')
+        room_1 = Room.new(:bedroom, 10, '13')
+        room_2 = Room.new(:bedroom, 11, '15')
+        room_3 = Room.new(:living_room, 25, '15')
+        room_4 = Room.new(:basement, 30, '41')
+        house.add_room(room_1)
+        house.add_room(room_2)
+        house.add_room(room_3)
+        house.add_room(room_4)
+
+        expect(house.rooms_from_category(:bedroom)).to eq([room_1, room_2])
+      end
     end
   end
 end
