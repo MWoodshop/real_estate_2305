8822bfdbc813bbd2367a4252e22929203a7d9df0
Mike Wood <mikewwood@icloud.com>
2023-05-18 12:38:07 -0400

    Test: add area method from House class test.

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -117,6 +117,20 @@ RSpec.describe Room do
 
         expect(house.rooms_from_category(:basement)).to eq([room_4])
       end
+
+      it 'adds four rooms and calculates house.area' do
+        house = House.new('$400000', '123 sugar lane')
+        room_1 = Room.new(:bedroom, 10, '13')
+        room_2 = Room.new(:bedroom, 11, '15')
+        room_3 = Room.new(:living_room, 25, '15')
+        room_4 = Room.new(:basement, 30, '41')
+        house.add_room(room_1)
+        house.add_room(room_2)
+        house.add_room(room_3)
+        house.add_room(room_4)
+
+        expect(house.area).to eq(1900)
+      end
     end
   end
 end
