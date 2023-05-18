fbb614c9395d158223a242f3b3004f385248f30b
Mike Wood <mikewwood@icloud.com>
2023-05-18 11:24:06 -0400

    Feature: add paint method.

--- lib/room.rb
+++ lib/room.rb
@@ -1,13 +1,18 @@
 class Room
-  attr_reader :category, :length, :width
+  attr_reader :category, :length, :width, :painted
 
   def initialize(category, length, width)
     @category = category
     @length = length
     @width = width
+    @painted = false
   end
 
   def area
     @length * @width.to_i
+  end
+
+  def paint
+    @painted = true
   end
 end
